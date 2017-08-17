9aa5fab29794aee6fffed3c087f898cd7e3794e1
SoonJooUh <madeforonebox@gmail.com>
2017-08-17 14:15:46 -0700

    minify files using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,64 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    
+    
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'build/index.min.html': 'index.html',     // 'destination': 'source'
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'build/',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'build/main.min.js': ['js/helper.js', 'js/resumeBuilder.js']
+        }
+      }
+    },
+
     responsive_images: {
       dev: {
         options: {
           engine: 'im',
           sizes: [{
             width: 800,
-            suffix: '_large_1x',
+            suffix: '',
             quality: 50
           }]
         },
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
-          cwd: 'images/',
-          dest: 'images/dist'
+          cwd: 'img/',
+          dest: 'build/img'
         }]
       }
     },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default',['responsive_images']);
+  grunt.registerTask('default',['htmlmin', 'cssmin', 'uglify', 'responsive_images']);
 
 };
